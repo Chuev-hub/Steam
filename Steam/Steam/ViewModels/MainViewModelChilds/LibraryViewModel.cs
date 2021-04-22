b1c9e15e681cb177a0b60c433e7dec7168321a32
Валера Коршун <korshunval2033@gmail.com>
2021-04-22 19:34:38 +0300

    ShopView

--- Steam/Steam/ViewModels/MainViewModelChilds/LibraryViewModel.cs
+++ Steam/Steam/ViewModels/MainViewModelChilds/LibraryViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Steam.BLL.DTO;
+using Steam.BLL.Services;
+using Steam.Infrastructure;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,18 @@ namespace Steam.ViewModels.MainViewModelChilds
 {
     class LibraryViewModel
     {
+        GameService gameService;
+        public LibraryViewModel(GameService gameService)
+        {
+            //gameService.GetAll().Where(x => x.)
+            //Account.CurrentAccount.AccountId;
+            //this.gameService = gameService;
+            //foreach (var item in gameService.GetAll())
+            //{
+            //    Games.Add(item);
+            //}
+            
+        }
+        public ObservableCollection<GameDTO> Games { get; set; } = new ObservableCollection<GameDTO>();
     }
 }
--- Steam/Steam/ViewModels/MainViewModelChilds/ShopViewModel.cs
+++ Steam/Steam/ViewModels/MainViewModelChilds/ShopViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Steam.BLL.DTO;
+using Steam.BLL.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,16 @@ namespace Steam.ViewModels.MainViewModelChilds
 {
     class ShopViewModel
     {
+        GameService gs;
+        public ShopViewModel(GameService gameService)
+        {
+            gs = gameService;
+            foreach (var item in gs.GetAll())
+            {
+                Games.Add(item);
+            }
+            
+        }
+        public ObservableCollection<GameDTO> Games { get; set; } = new ObservableCollection<GameDTO>();
     }
 }
