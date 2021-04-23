49de11f3e2d8c1cbd2a0b550dd538df60686046d
Nuril <korshunval2033@gmail.com>
2021-04-23 08:25:21 +0300

    Shop 25%

--- Steam/Steam/ViewModels/MainViewModelChilds/ShopViewModel.cs
+++ Steam/Steam/ViewModels/MainViewModelChilds/ShopViewModel.cs
@@ -1,26 +1,54 @@
 ﻿using Steam.BLL.DTO;
 using Steam.BLL.Services;
+using Steam.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Steam.ViewModels.MainViewModelChilds
 {
-    class ShopViewModel
+    class ShopViewModel : BaseNotifyPropertyChanged
     {
         GameService gs;
+        GameDTO curGame;
+        int gamePos = 0;
+        public GameDTO CurGame { get { return curGame; } set { curGame = value; Notify(); } }
         public ShopViewModel(GameService gameService)
         {
+            InitCommands();
             gs = gameService;
             foreach (var item in gs.GetAll())
             {
                 Games.Add(item);
             }
-            
+            CurGame = Games[gamePos];
+
         }
+
+        private void InitCommands()
+        {
+            Prev = new RelayCommand(x => {
+
+                if (gamePos == 0)
+                    gamePos = Games.Count;
+                CurGame = Games[--gamePos];
+
+            }); 
+            Next = new RelayCommand(x => {
+
+                if (gamePos == Games.Count-1)
+                    gamePos = -1;
+                CurGame = Games[++gamePos];
+
+            });
+        }
+
         public ObservableCollection<GameDTO> Games { get; set; } = new ObservableCollection<GameDTO>();
+        public ICommand Prev { get; set; }
+        public ICommand Next { get; set; }
     }
 }
